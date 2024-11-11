00883d688f5ed915b13020ecc1947620461f33d4
filipslunsky <fslunsky@gmail.com>
2024-11-11 01:02:27 +0100

    added graphical representation of history

--- client/src/features/habitLogs/History.tsx
+++ client/src/features/habitLogs/History.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../app/store.ts";
-import { useEffect } from "react";
 import { getLogs } from './state/slice.ts';
 
 interface HistoryProps {
-    habitId: number
-};
+    habitId: number;
+}
 
 const History: React.FC<HistoryProps> = ({ habitId }) => {
     const dispatch = useDispatch<AppDispatch>();
@@ -14,26 +14,35 @@ const History: React.FC<HistoryProps> = ({ habitId }) => {
 
     useEffect(() => {
         dispatch(getLogs(habitId));
-    }, [dispatch]);
+    }, [dispatch, habitId]);
+
+    const data = logs.map(log => ({
+        date: new Date(log.date).toISOString().split('T')[0],
+        y: 1
+    }));
 
     return (
         <>
             <h2>History</h2>
             {
-                logs.length > 0
-                ?
-                logs.map(log => {
-                    return (
-                        <div key={log.log_id}>
-                            {new Date(log.date).toISOString().split('T')[0]}
-                        </div>
-                    )
-                })
-                :
-                <p>there are no logs for this task yet</p>
+                logs.length > 0 ? (
+                    <ResponsiveContainer width="100%" height={200}>
+                        <ScatterChart>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis
+                                dataKey="date"
+                                type="category"
+                            />
+                            <YAxis dataKey="y" hide domain={[0, 2]} />
+                            <Scatter data={data} fill="#8884d8" />
+                        </ScatterChart>
+                    </ResponsiveContainer>
+                ) : (
+                    <p>There are no logs for this task yet</p>
+                )
             }
         </>
     );
-}
- 
+};
+
 export default History;
