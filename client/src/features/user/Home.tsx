f889a92fca4517669b10ca06825dcaaffaea7b3b
filipslunsky <fslunsky@gmail.com>
2024-11-12 17:56:49 +0100

    added favicon, styled logo, added logo to Home.tsx

--- client/src/features/user/Home.tsx
+++ client/src/features/user/Home.tsx
@@ -2,27 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
+import Logo from "./Logo";
+import './home.css';
 
 const Home: React.FC = () => {
     const isLoggedIn = useSelector((state: RootState) => state.user.loggedIn);
     return (
-    <>
-        <p>Start living your life better today.</p>
-        <p>Keep track of your good habits you want to strengthen.</p>
-        <p>Get rid of the bad ones effortlessly.</p>
-        <p>Take back the control over your life day-by-day, one good thing at a time.</p>
-        {
-            !isLoggedIn
-            ?
-            <div>
-                <h3>If you are new here, create an account and start today.</h3>
-                <Link to='/user/register'>CREATE ACCOUNT</Link>
-                <h3>Or you can log into your existing account.</h3>
-                <Link to='/user/login'>LOGIN</Link>
-            </div>
-            :
-            ''
-        }
+    <>  
+        <div className="main-home-container">
+            <Logo />
+            <p>Start living your life better today.</p>
+            <p>Keep track of your good habits you want to strengthen.</p>
+            <p>Get rid of the bad ones effortlessly.</p>
+            <p>Take back the control over your life day-by-day, one good thing at a time.</p>
+            {
+                !isLoggedIn
+                ?
+                <div>
+                    <h3>If you are new here, create an account and start today.</h3>
+                    <Link to='/user/register'>CREATE ACCOUNT</Link>
+                    <h3>Or you can log into your existing account.</h3>
+                    <Link to='/user/login'>LOGIN</Link>
+                </div>
+                :
+                ''
+            }
+        </div>
     </>
     );
 }
