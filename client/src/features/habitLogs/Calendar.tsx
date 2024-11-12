c63f092e3abd4f396c92c543572381ede0cc3d9d
filipslunsky <fslunsky@gmail.com>
2024-11-12 02:36:32 +0100

    added flexible range to Calendar.tsx

--- client/src/features/habitLogs/Calendar.tsx
+++ client/src/features/habitLogs/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../app/store.ts";
 import { getLogs } from './state/slice.ts';
@@ -11,6 +11,7 @@ interface CalendarProps {
 const Calendar: React.FC<CalendarProps> = ({ habitId }) => {
     const dispatch = useDispatch<AppDispatch>();
     const logs = useSelector((state: RootState) => state.logs.logs);
+    const [calendarRange, setCalendarRange] = useState(30);
 
     useEffect(() => {
         dispatch(getLogs(habitId));
@@ -25,16 +26,28 @@ const Calendar: React.FC<CalendarProps> = ({ habitId }) => {
     );
 
     const today = new Date();
-    const daysArray = Array.from({ length: 30 }, (_, i) => {
+    const daysArray = Array.from({ length: calendarRange }, (_, i) => {
         const date = new Date(today);
         date.setDate(today.getDate() - i);
         const utcDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
         return utcDate.toISOString().split('T')[0];
     }).reverse();
 
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setCalendarRange(parseInt(e.target.value, 10));
+    };
+
     return (
         <div className="calendar">
-            <h2>Last 30 Days Calendar</h2>
+            <h2>Last {calendarRange} Days</h2>
+            <select  className="range-selector-input" onChange={handleSelect} name="calendarRange">
+                <option value='' disabled selected>select range</option>
+                <option value={7}>last 7 days</option>
+                <option value={30}>last 30 days</option>
+                <option value={60}>last 60 days</option>
+                <option value={90}>last 90 days</option>
+                <option value={120}>last 120 days</option>
+            </select>
             <div className="calendar-grid">
                 {daysArray.map(date => (
                     <div
